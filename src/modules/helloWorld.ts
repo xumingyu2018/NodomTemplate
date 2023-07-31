44f56c82bacbafa90dea11f5198438c0568fa67e
xumingyu2018 <260602448@qq.com>
2023-07-31 14:00:34 +0800

    修改模版

--- src/modules/helloWorld.ts
+++ src/modules/helloWorld.ts
@@ -4,7 +4,7 @@ export default class Hello extends Module {
   template() {
     return `
         <div class='hello'>
-          <h1>Hello Nodom 3 + TypeScript </h1>
+          <h1>Nodom 3 + TypeScript </h1>
           <p>welcome to <a href='https://gitee.com/weblabsw/nodom3' target='_blank'>nodom</a></p>
           <button x-route='/main/top'>say,Hi!</button>
           <div x-router></div>

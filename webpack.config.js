4d530a0ab62e72d4b0825d18836049a717f72ece
xumingyu2018 <260602448@qq.com>
2023-08-16 15:19:26 +0800

    修改分块打包配置

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
           chunks: 'all',
           name: (module) => {
             // 生成拆分块名称：模块名称 + 哈希值的一部分
-            const moduleFileName = module.identifier().split('/').reduceRight(item => item);
+            const moduleFileName = path.basename(module.identifier(), path.extname(module.identifier()));
             const hash = require('crypto').createHash('md5').update(moduleFileName).digest('hex').substring(0, 8);
             return `${moduleFileName}_${hash}`;
           },
